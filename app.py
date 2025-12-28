e0137fb2058755a1146a0870711f8ecfb7e6ae60
sajesh1985 <sajesh_1985@yahoo.co.in>
2025-12-28 16:42:53 -0600

    Added Docker file

---
+++ app.py
@@ -0,0 +1,65 @@
+import json
+import boto3
+import time
+from datetime import datetime
+
+bedrock = boto3.client("bedrock-runtime")
+MODEL_ID = "anthropic.claude-3-sonnet-20240229-v1:0"
+
+
+def lambda_handler(event, context):
+    body = json.loads(event.get("body", "{}"))
+    message = body.get("message", "")
+
+    if not message:
+        return response(400, "Message is required")
+
+    result = bedrock.invoke_model(
+        modelId=MODEL_ID,
+        body=json.dumps({
+            "messages": [{"role": "user", "content": message}],
+            "max_tokens": 400
+        })
+    )
+
+    data = json.loads(result["body"].read())
+    reply = data["content"][0]["text"]
+
+    # Create DynamoDB client (IAM role auto-detected on EC2)
+    dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
+    table = dynamodb.Table("chat-response-d")
+
+    st.title("Save Message to DynamoDB")
+
+    user_id = st.text_input("User ID")
+    message = st.text_area("Message")
+
+   if st.button("Save"):
+    if not user_id or not message:
+        st.error("Please enter both user_id and message")
+    else:
+        item = {
+            "id": "678",
+            "interactionId": "3456885484dfsw3",
+            "input": message,
+            "metric":9,
+	    "response":reply
+
+        }
+
+        table.put_item(Item=item)
+
+        st.success("Message saved successfully!")
+
+
+    return response(200, reply)
+
+def response(code, msg):
+    return {
+        "statusCode": code,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps({"reply": msg})
+    }
